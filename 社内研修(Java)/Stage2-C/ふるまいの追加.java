6e326f4b2c8baa542ec785019feef39b62d3bcb3
Ryo-Sumiyama <63088619+Ryo-Sumiyama@users.noreply.github.com>
2020-04-13 15:26:23 +0900

    初回提出

---
+++ 社内研修(Java)/Stage2-C/ふるまいの追加.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("入力された値:" );
+    String text =scanner.next();
+    
+    System.out.println("入力された値は" + text + "です");
+    
+    int number = text.length();
+    System.out.println(number);
+    
+     System.out.println(text.charAt(0));
+     System.out.println(text.charAt(8));
+    
+    if (text.contains("A")){
+        System.out.println("Yes");
+    }
+        else{
+            System.out.println("No");
+        }
+  }
+  }
