f598779f3805a281e0c88221f95bcc11aa8b2502
Ryo-Sumiyama <63088619+Ryo-Sumiyama@users.noreply.github.com>
2020-04-13 15:43:24 +0900

    初回提出

---
+++ 社内研修(Java)/Stage2-D/配列の出力.java
@@ -0,0 +1,11 @@
+class Main {
+  public static void main(String[] args) {
+      
+      String[] fruits = {"orange","apple","cherry","grape","apple"};
+      System.out.println(fruits[0]);
+       System.out.println(fruits[1]);
+        System.out.println(fruits[2]);
+         System.out.println(fruits[3]);
+          System.out.println(fruits[4]);
+  }
+}
