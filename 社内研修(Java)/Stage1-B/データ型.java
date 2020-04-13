f856b710a50ebd2e4b9e39ae4cd29c8984a86be4
Ryo-Sumiyama <63088619+Ryo-Sumiyama@users.noreply.github.com>
2020-04-13 10:37:14 +0900

    初回提出

---
+++ 社内研修(Java)/Stage1-B/データ型.java
@@ -0,0 +1,29 @@
+import java.util.Date;
+
+public class main1b {
+  
+  public static void main(String[] args) {
+      
+    char alphabet=65;
+    
+    System.out.println(alphabet); 
+    
+    String name="sumiyama";
+    
+    System.out.println(name);
+    
+    int number=3;
+    
+    System.out.println(number); 
+    
+    boolean alternative=true;
+    
+    System.out.println(true); 
+    
+    
+    Date now = new Date();
+        
+    System.out.println(now);
+    
+  }
+}
