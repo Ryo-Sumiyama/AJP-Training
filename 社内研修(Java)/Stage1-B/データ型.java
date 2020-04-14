90315da4bc9320613df28193d9980a06e422f8bb
Ryo-Sumiyama <63088619+Ryo-Sumiyama@users.noreply.github.com>
2020-04-14 10:56:18 +0900

    3回目提出

--- 社内研修(Java)/Stage1-B/データ型.java
+++ 社内研修(Java)/Stage1-B/データ型.java
@@ -6,23 +6,22 @@ public class Main1b {
       
     char alphabet=65;
     
-    System.out.println(alphabet); 
-    
     String name="sumiyama";
-    
-    System.out.println(name);
     
     int number=3;
     
-    System.out.println(number); 
-    
     boolean alternative=true;
     
-    System.out.println(true); 
-    
-    
     Date now = new Date();
-        
+    
+    System.out.println(name);
+    
+    System.out.println(alphabet); 
+    
+    System.out.println(number); 
+    
+    System.out.println(alternative); 
+    
     System.out.println(now);
     
   }
