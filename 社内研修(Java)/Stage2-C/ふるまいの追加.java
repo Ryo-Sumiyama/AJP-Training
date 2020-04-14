58d80e5cdd6d69db0db83ec7641462c841ac24ca
Ryo-Sumiyama <63088619+Ryo-Sumiyama@users.noreply.github.com>
2020-04-14 14:36:58 +0900

    2回目提出

--- 社内研修(Java)/Stage2-C/ふるまいの追加.java
+++ 社内研修(Java)/Stage2-C/ふるまいの追加.java
@@ -6,19 +6,23 @@ class Main {
     System.out.println("入力された値:" );
     String text =scanner.next();
     
-    System.out.println("入力された値は" + text + "です");
+    
     
     int number = text.length();
-    System.out.println(number);
     
-     System.out.println(text.charAt(0));
-     System.out.println(text.charAt(8));
+    int cut_length = 1;
     
+     System.out.println("入力された値は{" + text + "}です");
+     System.out.println("桁数:{" + number + "}桁");
+     System.out.println("頭文字:{" + text.charAt(0) + "}");
+     System.out.println("語尾:{" +  text.substring(number - cut_length) + "}");
+    
+    //文字列にAを含むかどうか
     if (text.contains("A")){
-        System.out.println("Yes");
+        System.out.println("文字列にAを含む：{Yes}");
     }
         else{
-            System.out.println("No");
+            System.out.println("文字列にAを含む：{No}");
         }
-  }
+   }
   }
