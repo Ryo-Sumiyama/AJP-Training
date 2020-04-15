a8b23a0fa016eade82653723b53216674bf3c58d
Ryo-Sumiyama <63088619+Ryo-Sumiyama@users.noreply.github.com>
2020-04-15 14:21:23 +0900

    4回目提出

--- 社内研修(Java)/Stage2-D/配列の出力.java
+++ 社内研修(Java)/Stage2-D/配列の出力.java
@@ -2,7 +2,7 @@ class Main {
   public static void main(String[] args) {
       
       String S =  "orange/apple/cherry/grape/apple";
-      String[] H = {"orange","apple","cherry","grape","apple"};
+      String[] H = S.split("/", 0);
      
      for (int i = 0 ; i <H.length; i++) {
         System.out.println(i + 1 + ":"+H[i]);
