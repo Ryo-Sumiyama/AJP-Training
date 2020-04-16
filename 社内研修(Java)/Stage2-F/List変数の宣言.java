ebca138048074cf6dce2ff04b58c76eeefa128e1
Ryo-Sumiyama <63088619+Ryo-Sumiyama@users.noreply.github.com>
2020-04-16 16:28:20 +0900

    初回提出

---
+++ 社内研修(Java)/Stage2-F/List変数の宣言.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+public class Main {
+    public static void main(String[] args) {
+      List <String> L = new ArrayList<>(Arrays.asList());
+      
+      L.add("monday");
+      L.add("tuesday");
+      L.add("wednesday");
+      L.add("thursday");
+      L.add("friday");
+      L.add("saturday");
+      L.add("sunday");
+      
+      System.out.println(L);
+    }
+}
