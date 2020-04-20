bb071d60831f97a266e9c1a25c50d0aaa4cdbdaf
Ryo-Sumiyama <63088619+Ryo-Sumiyama@users.noreply.github.com>
2020-04-20 14:50:27 +0900

    2回目提出

--- 社内研修(Java)/Stage2-H/calenderクラスの利用.java
+++ 社内研修(Java)/Stage2-H/calenderクラスの利用.java
@@ -4,18 +4,18 @@ import java.text.SimpleDateFormat;
 class Main {
  
     public static void main(String[] args) {
-      Calendar calendar = Calendar.getInstance();
-     
-      int hour = calendar.get(Calendar.HOUR_OF_DAY);
-      int minute = calendar.get(Calendar.MINUTE);
-      int second = calendar.get(Calendar.SECOND);
-      
-      SimpleDateFormat now = new SimpleDateFormat("HH:mm:ss");
-      System.out.println(now.format(calendar.getTime()));
+        Calendar calendar = Calendar.getInstance();
+     //現在時刻を取得
+        int hour = calendar.get(Calendar.HOUR_OF_DAY);
+        int minute = calendar.get(Calendar.MINUTE);
+        int second = calendar.get(Calendar.SECOND);
+      //出力フォーマットを指定
+        SimpleDateFormat now = new SimpleDateFormat("HH:mm:ss");
+        System.out.println(now.format(calendar.getTime()));
        
-       for (int i = 1; i <= 19; i++) {
-        calendar.add(Calendar.SECOND,1);
-      System.out.println(now.format(calendar.getTime()));
+        for (int i = 1; i <= 19; i++) {
+            calendar.add(Calendar.SECOND,1);
+            System.out.println(now.format(calendar.getTime()));
         }
         
         System.out.println("20秒カウント終わりました");
