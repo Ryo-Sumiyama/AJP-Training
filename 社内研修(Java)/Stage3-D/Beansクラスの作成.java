38d296d4a1f6ebda710914f0094575db775f41de
Ryo-Sumiyama <63088619+Ryo-Sumiyama@users.noreply.github.com>
2020-04-28 17:58:41 +0900

    初回提出

---
+++ 社内研修(Java)/Stage3-D/Beansクラスの作成.java
@@ -0,0 +1,33 @@
+import java.io.Serializable;
+
+class Main {
+  
+  public static void main(String[] args) 
+    }
+  public class UserBean implements Serializable {
+    
+　  private static final long serialVersionUID = 1L;
+    
+  　private String userId;
+    private String userName;
+  　private String password;
+    private Date birthday;
+    private int age;
+    
+  　public UserBean() {}
+    //userIdのsetter、getter
+    public void setUserId(String userId) {this.userId = userId;}
+    public String getUserId() {return this.userId;}
+    //userNameのsetter、getter
+    public void setUserName(String userName) {this.userName = userName;}
+    public String getUserName() {return this.userName;}
+    //passwordのsetter、getter
+    public void setPassword(String password) {this.password = password;}
+    public String getPassword() {return this.password;}
+    //birthdayのsetter、getter
+    public void setBirthday(Date birthday) {this.birthday = birthday;}
+    public Date getBirthday() {return this.birthday;}
+    //ageのsetter、getter
+    public void setAge(int age) {this.age = age;}
+    public int getAge() {return this.age;}
+    }
