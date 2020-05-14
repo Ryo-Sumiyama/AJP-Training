9dc3bf7478ec0a9eb642266527cde6aa4fc51d93
Ryo-Sumiyama <63088619+Ryo-Sumiyama@users.noreply.github.com>
2020-05-14 18:00:40 +0900

    4回目提出

--- 社内研修(Java)/Stage2-E/重複の排除.java
+++ 社内研修(Java)/Stage2-E/重複の排除.java
@@ -1,46 +1,24 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 class Main {
   public static void main(String[] args) {
       //文字列変数fruitsを宣言
       String fruits =  "orange/apple/cherry/orange/grape/apple";
       //配列変数fruits2に変換
       String[] fruits2 = fruits.split("/", 0);
+     
+      List<String> list = new ArrayList<String>();
      //変数fruits2から重複している値を取得
-      String dup = "";
-     
-      for (int i = 1 ; i <fruits2.length; i++) {
-          if (fruits2[0].equals(fruits2[i])){
-              dup = fruits2[0];
-        }  
-      }
-     
-      for (int i = 2 ; i <fruits2.length; i++) {
-          if (fruits2[1].equals(fruits2[i])){
-              dup = dup + "/" + fruits2[1];
-        }  
-      }
-     
-      for (int i = 3 ; i <fruits2.length; i++) {
-          if (fruits2[2].equals(fruits2[i])){
-              dup = dup + "/" + fruits2[2];
-        }
-      }
-     
-      for (int i = 4 ; i <fruits2.length; i++) {
-          if (fruits2[3].equals(fruits2[i])){
-              dup = dup + "/" + fruits2[3];
-        }
-      }
-     
-      for (int i = 5 ; i <fruits2.length; i++) {
-          if (fruits2[4].equals(fruits2[i])){
-              dup = dup + "/" + fruits2[4];
-        }
-      }
-      String[] dup2 = dup.split("/", 0);
-     
-      for (int i = 0 ; i <dup2.length; i++) {
-          System.out.println(i + 1 + ":" + dup2[i]);
-      }
-   
-  }
+      for (int i = 0 ; i < 5 ; i++) {
+        for(int i2 = 1 ; i2 < fruits2.length; i2++)
+        if (fruits2[i].equals(fruits2[i2]) && i != i2 ){
+          list.add(fruits2[i]);}
+       }
+       //重複した値を出力
+       String[] array=(String[])list.toArray(new String[0]);
+       System.out.println( 1 + ":" +array[0]);
+       System.out.println( 2 + ":" +array[1]);
+    }
 }
