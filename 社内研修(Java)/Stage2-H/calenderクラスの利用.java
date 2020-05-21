dacbe6b5e77a9d3016c48bc051e17fde8922b9a7
Ryo-Sumiyama <63088619+Ryo-Sumiyama@users.noreply.github.com>
2020-05-21 13:40:16 +0900

    3回目提出

--- 社内研修(Java)/Stage2-H/calenderクラスの利用.java
+++ 社内研修(Java)/Stage2-H/calenderクラスの利用.java
@@ -1,23 +1,31 @@
 import java.util.Calendar;
 import java.text.SimpleDateFormat;
+import java.util.Timer; 
+import java.util.TimerTask;
 
-class Main {
+class Stage2H {
  
     public static void main(String[] args) {
         Calendar calendar = Calendar.getInstance();
-     //現在時刻を取得
-        int hour = calendar.get(Calendar.HOUR_OF_DAY);
-        int minute = calendar.get(Calendar.MINUTE);
-        int second = calendar.get(Calendar.SECOND);
+        
       //出力フォーマットを指定
         SimpleDateFormat now = new SimpleDateFormat("HH:mm:ss");
-        System.out.println(now.format(calendar.getTime()));
-       
-        for (int i = 1; i <= 19; i++) {
-            calendar.add(Calendar.SECOND,1);
-            System.out.println(now.format(calendar.getTime()));
-        }
+        System.out.println("20秒カウントスタート");
         
-        System.out.println("20秒カウント終わりました");
+         Timer timer = new Timer();
+         TimerTask task = new TimerTask() {
+             int cnt = 0;
+             public void run() {
+                 calendar.add(Calendar.SECOND,1);
+                 System.out.println(now.format(calendar.getTime()));
+                 cnt++;
+                 if (cnt >= 20) {
+                     timer.cancel();
+                     System.out.println("20秒カウント終わりました");
+                 }
+        }
+         };
+          timer.scheduleAtFixedRate(task,1000,1000);
+       
     }
 }
