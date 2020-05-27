8261a869e1b27a0431ecb1e0436bb21c54c90f25
Ryo-Sumiyama <63088619+Ryo-Sumiyama@users.noreply.github.com>
2020-05-27 17:30:26 +0900

    2回目提出

--- 社内研修(Java)/Stage3-A/メソッドの作成.java
+++ 社内研修(Java)/Stage3-A/メソッドの作成.java
@@ -1,13 +1,13 @@
-public class Main {
+public class Stage3A {
    //メンバ変数piを宣言
   static double pi =3.14;
   public static void main(String[] args) {
     
-		 double result = area(5.5, pi);
+		 double result = area(5.5);
 		 System.out.println(result);
 	}
  //privateメソッドareaを作成
-	private static double area(double radius, double pi) {
+	private static double area(double radius) {
 		  double mul = radius * radius * pi;
 	  	return mul;
 	}
