4ed6354d777a9b0e1e83b094c515c0cda962ee7d
Ryo-Sumiyama <63088619+Ryo-Sumiyama@users.noreply.github.com>
2020-05-27 17:37:04 +0900

    3回目提出

--- 社内研修(Java)/Stage3-B/ファイルへの書き込み出力.java
+++ 社内研修(Java)/Stage3-B/ファイルへの書き込み出力.java
@@ -1,16 +1,19 @@
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
-class Main {
+class Stage3B {
     public static void main (String[] args) {
         printColors("red","blue","yellow","green");
     }
     
     private static void printColors(String ... colors) {
-    
+        Date date = new Date();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyMMdd");
         try {
-        File file = new File("/home/ec2-user/environment/サンプル実行/output/{200423}_output.txt");
+        File file = new File("/home/ec2-user/environment/サンプル実行/output/{" + sdf.format(date) + "}_output.txt");
         FileWriter fw = new FileWriter(file);
         for (int i = 0; i < colors.length; i++) {
              fw.write(colors[i]);
