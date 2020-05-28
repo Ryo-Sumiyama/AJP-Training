01eecbb525cb1d2f36e44ac6e341406ceab7deab
Ryo-Sumiyama <63088619+Ryo-Sumiyama@users.noreply.github.com>
2020-05-28 10:08:34 +0900

    2回目提出

--- 社内研修(Java)/Stage3-F/フィールドの追加.java
+++ 社内研修(Java)/Stage3-F/フィールドの追加.java
@@ -20,21 +20,40 @@ import java.text.ParseException;
     
     UserBean() {}
     //userIdのsetter、getter
-    public void setUserId(String userId) {this.userId = userId;}
-    public String getUserId() {return this.userId;}
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+    public String getUserId() {
+        return this.userId;
+    }
     //userNameのsetter、getter
-    public void setUserName(String userName) {this.userName = userName;}
-    public String getUserName() {return this.userName;}
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+    public String getUserName() {
+        return this.userName;
+    }
     //passwordのsetter、getter
-    public void setPassword(String password) {this.password = password;}
-    public String getPassword() {return this.password;}
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getPassword() {
+        return this.password;
+    }
     //birthdayのsetter、getter
-    public void setBirthday(Date birthday) {this.birthday = birthday;}
-    public Date getBirthday() {return this.birthday;}
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+    public Date getBirthday() {
+        return this.birthday;
+    }
     //ageのsetter、getter
-    public void setAge(int age) {this.age = age;}
-    public int getAge() {return this.age;}
-    //residenceのsetter,getter
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public int getAge() {
+        return this.age;
+        }
     //初期値に所属地を割りあてるコンストラクタを作成、インスタンスの生成数をカウント
     UserBean(String residence) {
         Main.cnt++;
@@ -44,13 +63,15 @@ import java.text.ParseException;
     //日付フォーマットを指定
     SimpleDateFormat now = new SimpleDateFormat("yyyy'年'MM'月'dd'日'");
     //toStringメソッドをオーバーライド
-     @Override 
-     public String toString() {
+    @Override 
+    public String toString() {
         return "ユーザID：{" + this.getUserId() + "}\r\nユーザ名：{" + this.getUserName() + "}\r\nパスワード：{" + this.getPassword() +  "}\r\n生年月日：{" + now.format(this.getBirthday()) + "}\r\n年齢：{" + this.getAge() + "}\r\n居住地：{" + this.residence + "}";
-        }
+    }
         
         //getCntメソッドを作成
-        public  int getCnt(){return Main.cnt;}
+        public  int getCnt(){
+        return Main.cnt;
+    }
         }
           }
 
@@ -68,11 +89,9 @@ import java.text.ParseException;
      userBean.setAge(23);
      
      try {
-           String strDate = "1997/05/26";
-         
-            SimpleDateFormat sdFormat = new SimpleDateFormat("yyyy/MM/dd");
-            Date date = sdFormat.parse(strDate);
-          
+        String strDate = "1997/05/26";
+        SimpleDateFormat sdFormat = new SimpleDateFormat("yyyy/MM/dd");
+        Date date = sdFormat.parse(strDate);
         userBean.setBirthday(date);
         } catch (ParseException e) {
             e.printStackTrace();
@@ -80,7 +99,6 @@ import java.text.ParseException;
      
     //セットした値を出力
     System.out.println(userBean.toString());
-    System.out.println(userBean);
     System.out.println("カウント数:{" + userBean.getCnt() + "}");
     }
 }
