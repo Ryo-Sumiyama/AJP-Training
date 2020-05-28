557cd1ebc4c074c6d6ab9aa3b5c9eae4e90a1a4a
Ryo-Sumiyama <63088619+Ryo-Sumiyama@users.noreply.github.com>
2020-05-28 09:36:05 +0900

    4回目提出

--- 社内研修(Java)/Stage3-B/ファイルへの書き込み出力.java
+++ 社内研修(Java)/Stage3-B/ファイルへの書き込み出力.java
@@ -1,17 +1,22 @@
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collections;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-class Stage3B {
+class Stage3C {
     public static void main (String[] args) {
         printColors("red","blue","yellow","green");
     }
     
     private static void printColors(String ... colors) {
+        //現在の日付を取得
         Date date = new Date();
+        //日付フォーマットを指定
         SimpleDateFormat sdf = new SimpleDateFormat("yyMMdd");
+        //テキストファイルを作成
         try {
         File file = new File("/home/ec2-user/environment/サンプル実行/output/{" + sdf.format(date) + "}_output.txt");
         FileWriter fw = new FileWriter(file);
@@ -21,12 +26,14 @@ class Stage3B {
         fw.close();
           } catch (IOException ex) {
                 ex.printStackTrace();
+                 
         }
-       
+        //5ファイル以上存在する場合、過去のファイルを削除
+        File dir = new File("/home/ec2-user/environment/サンプル実行/output");
+        File[] f = dir.listFiles();
+        Arrays.sort(f, Collections.reverseOrder());
+        if(f.length >= 5){
+        f[4].delete();
         }
-       	
     }
-
-        
-
-        
+}
