045b6ffa85707dd463a4cf19e2501f8e01bb07ae
Ryo-Sumiyama <63088619+Ryo-Sumiyama@users.noreply.github.com>
2020-06-05 14:00:07 +0900

    2回目提出

--- 社内研修(Java)/Stage3-E/インスタンスの作成.java
+++ 社内研修(Java)/Stage3-E/インスタンスの作成.java
@@ -1,15 +1,12 @@
-//Main.java
 import java.io.Serializable;
 import java.util.Date;
 import java.text.SimpleDateFormat;
 import java.text.ParseException;
 
   class Main {
-  
-    public static void main(String[] args){
-    }
-    public class UserBean implements Serializable {
     
+    public class UserBean implements Serializable {
+    UserBean(int cnt) {}
     private static final long serialVersionUID = 1L;
     
     private String userId;
@@ -17,58 +14,77 @@ import java.text.ParseException;
     private String password;
     private Date birthday;
     private int age;
+    private String residence;
     
-    public UserBean() {}
+    UserBean() {}
     //userIdのsetter、getter
-    public void setUserId(String userId) {this.userId = userId;}
-    public String getUserId() {return this.userId;}
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+    public String getUserId() {
+        return this.userId;
+    }
     //userNameのsetter、getter
-    public void setUserName(String userName) {this.userName = userName;}
-    public String getUserName() {return this.userName;}
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+    public String getUserName() {
+        return this.userName;
+    }
     //passwordのsetter、getter
-    public void setPassword(String password) {this.password = password;}
-    public String getPassword() {return this.password;}
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getPassword() {
+        return this.password;
+    }
     //birthdayのsetter、getter
-    public void setBirthday(Date birthday) {this.birthday = birthday;}
-    public Date getBirthday() {return this.birthday;}
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+    public Date getBirthday() {
+        return this.birthday;
+    }
     //ageのsetter、getter
-    public void setAge(int age) {this.age = age;}
-    public int getAge() {return this.age;}
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public int getAge() {
+        return this.age;
+        }
+    
     //日付フォーマットを指定
     SimpleDateFormat now = new SimpleDateFormat("yyyy'年'MM'月'dd'日'");
     //toStringメソッドをオーバーライド
-     @Override
-     public String toString() {
+    @Override 
+    public String toString() {
         return "ユーザID：{" + this.getUserId() + "}\r\nユーザ名：{" + this.getUserName() + "}\r\nパスワード：{" + this.getPassword() +  "}\r\n生年月日：{" + now.format(this.getBirthday()) + "}\r\n年齢：{" + this.getAge() + "}" ;
+    }
         }
           }
-}
 
-//Output.java
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
- class Output{ 
+ public class Stage3E{ 
    
    public static void main(String[] args){
+     
      Main a = new Main();
+     
      Main.UserBean userBean = a.new UserBean(); 
      //インスタンスに値をセット
      userBean.setUserId("1234"); 
      userBean.setUserName("炭山諒");
      userBean.setPassword("5678");
-     userBean.setAge(22);
+     userBean.setAge(23);
+     
      try {
-           String strDate = "1997/05/26";
-         
-            SimpleDateFormat sdFormat = new SimpleDateFormat("yyyy/MM/dd");
-            Date date = sdFormat.parse(strDate);
-          
+        String strDate = "1997/05/26";
+        SimpleDateFormat sdFormat = new SimpleDateFormat("yyyy/MM/dd");
+        Date date = sdFormat.parse(strDate);
         userBean.setBirthday(date);
         } catch (ParseException e) {
             e.printStackTrace();
         }
+     
     //セットした値を出力
     System.out.println(userBean.toString());
     }
