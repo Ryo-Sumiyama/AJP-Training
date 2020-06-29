c553a467504bb965442b4b89c036af7fbce3efe0
Ryo-Sumiyama <63088619+Ryo-Sumiyama@users.noreply.github.com>
2020-06-29 16:21:18 +0900

    3回目提出

--- 社内研修(Java)/ブラックジャック/ブラックジャックを作ろう.java
+++ 社内研修(Java)/ブラックジャック/ブラックジャックを作ろう.java
@@ -2,38 +2,69 @@ import java.security.SecureRandom;
 import java.util.Scanner;
 
 public class Black {
+    public static String answer;
+    public static int player;
+    public static int dealer;
+    public static int card;
     
     public static void main(String[] args) {
-     int player = 0;
-    //  プレイヤーの値(1-10)をランダムで取得、出力
+    //  startメソッドの呼び出し  
+        start();
+        
+        // ゲームが終了するまでループ
+        while(true){
+            
+        // inputメソッドの呼び出し
+        input();
+        
+        // yesメソッドの呼び出し
+        if("Y".equals(answer)){
+            yes();
+        }
+        
+         // noメソッドの呼び出し
+        if("N".equals(answer)){
+            no();
+            return;
+            }
+        }
+    }
+    
+    //  プレイヤーの値(1-10)をランダムで取得、出力するメソッド
+    public static void start(){
         SecureRandom card = new SecureRandom();
         int first = card.nextInt(10) + 1;
         player += first;
         System.out.println("『数値：[" + player + "]　合計：[" + player + "]』");
-        
-        while(true){
-        System.out.println("次のランダム数値を取得しますか[Y/N]");
-        // 文字を入力してもらう
-        Scanner scanner = new Scanner(System.in);
-        String answer =scanner.next();
-        
-        // Yの時
-        if("Y".equals(answer)){
-            int second = card.nextInt(10) + 1;
-            player += second;
-            System.out.println("『数値：[" + second + "]　合計：[" + player + "]』");
-            if(player > 21){
-                System.out.println("LOSE");
-               return;
-            }
-        }
+    }
     
-        // Nの時
-       if("N".equals(answer)){
+    // 次の値を取得するか聞き、入力してもらうメソッド
+    public static void input(){
+        System.out.println("次のランダム数値を取得しますか[Y/N]");
+        Scanner scanner = new Scanner(System.in);
+        answer =scanner.next();
+    } 
+    
+    // Yの時に動作するメソッド
+    public static void yes(){
+        // 新たに引いたカードの値を追加し出力
+        SecureRandom nextCard = new SecureRandom();
+        int second = nextCard.nextInt(10) + 1;
+        player += second;
+        System.out.println("『数値：[" + second + "]　合計：[" + player + "]』");
+            // バーストした時
+        if(player > 21){
+            System.out.println("LOSE");
+            System.exit(0);
+            }
+    }
+    
+    // Nの時に動作するメソッド
+    public static void no(){
         //   親の値(15-22)を取得、出力
-           SecureRandom dealerCard = new SecureRandom();
-           int dealer = dealerCard.nextInt(8) + 15;
-           System.out.println("『親数値：[" + dealer + "]　合計：[" + dealer + "]』");
+       SecureRandom dealerCard = new SecureRandom();
+       dealer = dealerCard.nextInt(8) + 15;
+       System.out.println("『親数値：[" + dealer + "]　合計：[" + dealer + "]』");
            
     //   親がバーストした時
        if(dealer > 21){
@@ -45,6 +76,7 @@ public class Black {
            System.out.println("LOSE");
            return;
        }
+    //   親＝プレイヤーの時
        else if(player==dealer){
            System.out.println("DRAW");
            return;
@@ -53,8 +85,7 @@ public class Black {
        else if(player > dealer){
            System.out.println("WIN");
            return;
+            }
        }
-       }
-    }
-    }
+
 }
